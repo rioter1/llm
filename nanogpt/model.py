1a273b4bd1b67acc36313e30dec505ef64b7e651
rioter1 <prateek.gsharma@gmail.com>
2024-07-18 09:58:13 +0530

    adding nanogpt/model.py

--- nanogpt/model.py
+++ nanogpt/model.py
@@ -10,6 +10,15 @@ class LayerNorm(nn.Module):
     """
     Layernormalization is a method where all normlaization of a matrix happens column wise. 
     for example in the attention matrix, the output is normlaized column wise
+    
+    Layernorm for a simple implemetation would only need 1 argument i.e. input
+    but here the the input_shape is needed to determine the correctness
+    of dimension, as in LLMs the input size is varying and this helps
+    with working with variable size matrices
+
+    the weights and bias are needed to learn a more flexible affine transformation
+    rather than using the 
+    
     """
     
     def __init__(self, ndim, bias):
@@ -21,3 +30,11 @@ class LayerNorm(nn.Module):
     def forward(self, input):
 
         return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
+
+class CausalSelfAttention(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_embd == 0
+
+        # key, query, value 
