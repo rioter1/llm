787abf84f0a0f8c8c973993da86be9457977110f
rioter1 <prateek.gsharma@gmail.com>
2024-07-26 18:34:25 +0530

    added nn forward pass

--- nanogpt/model.py
+++ nanogpt/model.py
@@ -208,8 +208,11 @@ class GPT(nn.Module):
         # report number of parameters
         print("number of parameters: %.2fM" % (self.get_num_params()/1e6,))
 
-        # This is a common technique to scale the initialization based on the number of layers, 
-        # which can help stabilize training.projection layers in transformers) may require a different scale of initialization to ensure effective learning.
+        # This is a common technique to scale the initialization
+        # based on the number of layers, 
+        # which can help stabilize training.projection layers in transformers) 
+        # may require a different scale of initialization to ensure 
+        # effective learning.
         # This is particularly relevant in deep networks, 
         # where the depth can lead to issues like vanishing or 
         # exploding gradients. Adjusting the initialization 
@@ -233,3 +236,31 @@ class GPT(nn.Module):
                 torch.nn.init.zeros_(module.bias)
         elif isinstance(module, nn.Embedding):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+
+    def forward(self, idx, targets=None):
+        device = idx.device
+        b, t = idx.size()
+        assert t <= self.config.block_size, f"Cannot forward sequence of length {t}, block size is only {self.config.block_size}"
+        pos = torch.arange(0, t, dtype=torch.long, device=device) # shape (t)
+        
+
+        # self.transformer was a moduledict object and the keys can be called as below
+        # forward the GPT model itself
+        tok_emb = self.transformer.wte(idx) # token embeddings of shape (b, t, n_embd)
+        pos_emb = self.transformer.wpe(pos) # position embeddings of shape (t, n_embd)
+        x = self.transformer.drop(tok_emb + pos_emb)
+        for block in self.transformer.h:
+            x = block(x)
+        x = self.transformer.ln_f(x)
+
+        if targets is not None:
+            # if we are given some desired targets also calculate the loss
+            logits = self.lm_head(x)
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
+        else:
+            # inference-time mini-optimization: only forward the lm_head on the very last position
+            logits = self.lm_head(x[:, [-1], :]) # note: using list [-1] to preserve the time dim
+            loss = None
+
+        return logits, loss
