610243266cdfe94c155b4665590862b85bb6044c
rioter1 <prateek.gsharma@gmail.com>
2024-07-30 10:43:18 +0530

    added optimizer code

--- nanogpt/model.py
+++ nanogpt/model.py
@@ -320,6 +320,10 @@ class GPT(nn.Module):
         sd_keys = sd.keys()
         # the pre-trained attention bias is not loaded, allowing the 
         # model to learn its own attention bias during fine-tuning or training from scratch.
+        # Dropping these keys ensures that there is no mismatch between the 
+        # pre-trained values and the model's expected initialization 
+        # for these parameters.
+
         sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # discard this mask / buffer, not a param
 
         # init a huggingface/transformers model
@@ -349,3 +353,64 @@ class GPT(nn.Module):
                     sd[k].copy_(sd_hf[k])
 
         return model
+    
+    def configure_optimizers(self, weight_decay, learning_rate, betas, device_type):
+        # start with all of the candidate parameters
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        # filter out those that do not require grad
+        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+        # create optim groups. Any parameters that is 2D will be weight decayed, otherwise no.
+        # i.e. all weight tensors in matmuls + embeddings decay, all biases and layernorms don't.
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+        optim_groups = [
+            {'params': decay_params, 'weight_decay': weight_decay},
+            {'params': nodecay_params, 'weight_decay': 0.0}
+        ]
+
+        '''
+        Weight decay is a commonly used regularization technique that helps prevent 
+        overfitting by adding a penalty term to the loss function. 
+        It encourages the model to learn smaller weights, which can lead to better generalization
+        
+        The rationale behind this separation is that weight decay is typically applied
+        to the weight tensors (e.g., in fully connected layers, 
+        convolutional layers, or embeddings) to encourage sparsity and 
+        prevent overfitting. However, it is often not applied to biases and layer norms, 
+        as they have different properties and may not benefit from weight decay in the 
+        same way.
+        '''
+        
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
+        print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+        print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+        # Create AdamW optimizer and use the fused version if it is available
+        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and device_type == 'cuda'
+        extra_args = dict(fused=True) if use_fused else dict()
+        optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=betas, **extra_args)
+        print(f"using fused AdamW: {use_fused}")
+
+        return optimizer
+
+
+    # since i have an rtx 3060 gpu whereas karapathy has an A100 gpu, 
+    # i am modifying the flop estimation code to work with for 3060 instaed
+
+    def estimate_mfu(self, fwdbwd_per_iter, dt):
+        """ estimate model flops utilization (MFU) in units of RTX 3060 FP16 peak FLOPS """
+        # first estimate the number of flops we do per iteration.
+        # see PaLM paper Appendix B as ref: https://arxiv.org/abs/2204.02311
+        N = self.get_num_params()
+        cfg = self.config
+        L, H, Q, T = cfg.n_layer, cfg.n_head, cfg.n_embd // cfg.n_head, cfg.block_size
+        flops_per_token = 6 * N + 12 * L * H * Q * T
+        flops_per_fwdbwd = flops_per_token * T
+        flops_per_iter = flops_per_fwdbwd * fwdbwd_per_iter
+        # express our flops throughput as ratio of RTX 3060 FP16 peak flops
+        flops_achieved = flops_per_iter * (1.0 / dt)  # per second
+        flops_promised = 60e12  # RTX 3060 FP16 peak flops is 60 TFLOPS
+        mfu = flops_achieved / flops_promised
+        return mfu
+
